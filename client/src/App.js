0b1361af314178f05fbf8877f67dff5077f906ba
shubhammehra4 <shubhammehra4@gmail.com>
2021-01-20 16:54:03 +0530

    Initial Client Setup 🌟

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+    return (
+        <Router>
+            <div></div>
+        </Router>
+    );
+}
+
+export default App;
